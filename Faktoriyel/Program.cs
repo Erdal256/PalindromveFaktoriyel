c7d1dd4ccc115b257ae81b0f134430f360292e83
Erdal256 <erdall.2693@gmail.com>
2020-11-02 23:11:02 +0300

    Add project files.

---
+++ Faktoriyel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktoriyel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, i, f = 1;
+            Console.WriteLine("Faktoriyeli sayı giriniz:");
+            sayi = Convert.ToInt16(Console.ReadLine());
+            for (i = 1; i <= sayi; i++)
+            {
+
+                f = i * f;
+
+            }
+            Console.WriteLine("Cevap : " + f);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Palindrom/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+            int sayi, rem, sum = 0, gcc;
+            
+            Console.Write(" Sayı Giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            gcc = sayi;
+            while (sayi > 0)
+            {
+                rem = sayi % 10;
+                sayi = sayi / 10;
+                sum = sum * 10 + rem;
+            }
+            if (gcc == sum)
+            {
+                Console.WriteLine(" Sayı Palindrom'dur.");
+            }
+            else
+            {
+                Console.WriteLine(" Sayı Palindrom Değil.");
+            }
+            
+            ConsoleKeyInfo tus = new ConsoleKeyInfo();
+            do
+            {
+                tus = Console.ReadKey(true);
+               
+                Console.WriteLine("'{0}' harfine bastınız.", tus.Key);
+            } while (tus.Key != ConsoleKey.Q);
+
+        }
+
+    }
+}
+
+
